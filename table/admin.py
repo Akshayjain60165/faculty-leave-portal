efac240585a00fdcdf5858f478162499c9def504
Akshayjain60165 <69035152+Akshayjain60165@users.noreply.github.com>
2021-05-13 15:30:40 +0530

    Add files via upload

---
+++ table/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+
+
+from table.models import (login_table, login_time, num_of_leaves, cur_running,
+                              app_status, cse_dep, ee_dep, mec_dep, hoddean,education,course,
+                              cse_hod, mec_hod, ee_hod, hod_dean, director, dean, hodapp, deanapp, dirapp,proffesor,educationproff,courseproff)
+
+admin.site.register(login_table)
+admin.site.register(login_time)
+admin.site.register(num_of_leaves)
+admin.site.register(cur_running)
+admin.site.register(app_status)
+admin.site.register(cse_dep)
+admin.site.register(ee_dep)
+admin.site.register(mec_dep)
+admin.site.register(hoddean)
+admin.site.register(cse_hod)
+admin.site.register(mec_hod)
+admin.site.register(ee_hod)
+admin.site.register(hod_dean)
+admin.site.register(director)
+admin.site.register(dean)
+admin.site.register(hodapp)
+admin.site.register(deanapp)
+admin.site.register(dirapp)
+admin.site.register(proffesor)
+admin.site.register(education)
+admin.site.register(course)
+admin.site.register(educationproff)
+admin.site.register(courseproff)
